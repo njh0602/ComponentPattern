879d3167def8ae08d94cb0d2083d8401e21a7423
Jun Hyeon, Nam <njh0602@naver.com>
2023-12-29 18:53:53 +0900

    Create Component.hpp

---
+++ Component.hpp
@@ -0,0 +1,34 @@
+#include <type_traits>
+#include <concepts>
+
+template<typename Owner>
+class ComponentBase
+{
+public:
+	ComponentBase(Owner& owner) : _owner(owner)
+	{
+	}
+
+	Owner& getOwner()
+	{
+		return _owner;
+	}
+
+protected:
+	Owner& _owner{};
+};
+
+template<typename Owner, typename ... Components>
+class ComponentOwner : public Components ...
+{
+public:
+	ComponentOwner(Owner& owner) : Components(owner)...
+	{
+	}
+
+	template<typename Component> requires std::disjunction_v<std::is_same<Component, Components>...>
+	Component& getComponent()
+	{
+		return static_cast<Component&>(*this);
+	}
+};
